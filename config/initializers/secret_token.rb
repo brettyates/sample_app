e5d45b7ecff6513c87c0b4f6a58507cfb2b76590
Brett Yates <brettyates@gmail.com>
2013-12-26 13:48:45 -0700

    installed rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'eb9058c6daef31c8a85a2572d6d8594e96adfe29254df71c4904c3b398c1aa7986203bd3a54baec75086dbbb29261aa8b36ae49d720361b25784386195fd66cd'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
